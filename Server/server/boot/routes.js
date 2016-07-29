153955e72c7d65e5adb7b6d5aadb7e2533babb1e
Khanh Pham <vjtc0n@Vjtc0ns-MacBook-Pro.local>
2016-07-29 15:53:47 +0900

    update model

--- Server/server/boot/routes.js
+++ Server/server/boot/routes.js
@@ -7,5 +7,51 @@ module.exports = function (app) {
   router.get('/ping', function(req, res) {
     res.send('pongaroo');
   });
+
+  app.post('/login', function(req, res) {
+    User.login({
+      email: req.body.email,
+      password: req.body.password
+    }, 'user', function(err, token) {
+      if (err) {
+        res.render('response', { //render view named 'response.ejs'
+          title: 'Login failed',
+          content: err,
+          redirectTo: '/',
+          redirectToLinkText: 'Try again'
+        });
+        return;
+      }
+
+      res.render('home', { //login user and render 'home' view
+        email: req.body.email,
+        accessToken: token.id
+      });
+    });
+  });
+
+
+  app.get('/logout', function(req, res, next) {
+    if (!req.accessToken) return res.sendStatus(401); //return 401:unauthorized if accessToken is not present
+    User.logout(req.accessToken.id, function(err) {
+      if (err) return next(err);
+      res.redirect('/'); //on successful logout, redirect
+    });
+  });
+
+  app.post('/request-password-reset', function(req, res, next) {
+    User.resetPassword({
+      email: req.body.email
+    }, function(err) {
+      if (err) return res.status(401).send(err);
+      res.render('response', {
+        title: 'Password reset requested',
+        content: 'Check your email for further instructions',
+        redirectTo: '/',
+        redirectToLinkText: 'Log in'
+      });
+    });
+  });
+
   app.use(router);
 };
