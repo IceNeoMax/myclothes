211508e1dd7ac46b1067546a986339df8b748f44
Khanh Pham <vjtc0n@Vjtc0ns-MacBook-Pro.local>
2016-09-21 21:08:22 +0700

    update server

--- Server/common/models/member.js
+++ Server/common/models/member.js
@@ -1,3 +1,34 @@
+'use strict'
+
+var loopback = require('loopback');
+
+
 module.exports = function(Member) {
+
+  Member.searchForMember = function (user_name, cb) {
+    Member.find({
+      where: {
+        user_name: user_name
+      },
+      fields: {
+        email: false
+      }}, function (error, members) {
+      if (error) cb(error);
+      if (!error) {
+        cb(null, members)
+      }
+    });
+  };
+
+  Member.remoteMethod(
+    'searchForMember',
+    {
+      http: {path: '/searchformember', verb: 'get'},
+      accepts: {arg: 'user_name', type: 'string', http: { source: 'query' } },
+      returns: [
+        {arg: 'members', type: 'object'}
+      ]
+    }
+  );
 
 };
