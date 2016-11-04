36071fd976a84ede915fe2c24678ca6a86d78f4c
Khanh Pham <vjtc0n@Vjtc0ns-MacBook-Pro.local>
2016-11-04 21:14:41 +0700

    update productpage

--- React-native/Myclothes/src/Comment/containers/CommentModal.js
+++ React-native/Myclothes/src/Comment/containers/CommentModal.js
@@ -5,45 +5,35 @@ import React, { Component } from 'react';
 import {
     Dimensions,
     StyleSheet,
-    ScrollView,
+    TouchableOpacity,
     View,
     Image,
     Text,
-    ListView
+    ListView,
+    TextInput
 } from 'react-native';
 
 import ButtonAPSL from 'apsl-react-native-button'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import Modal from 'react-native-modalbox'
+import ViewMoreText from 'react-native-view-more-text';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 const window = Dimensions.get('window');
 
-DATA = [
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'},
-    {text: 'ABC'}
-];
+DATA = [];
+
+for (var i=0; i<=10; i++) {
+    DATA.push({
+        img: 'http://static.zerochan.net/Yuuki.Asuna.full.2001827.jpg',
+        name: 'Khanh',
+        comment: 'Thường trực Tỉnh ủy đang xem xét, giao cơ' +
+        ' quan liên quan làm rõ trách nhiệm các cá nhân, địa phương đối với quá trình giải quyết vụ ' +
+        'việc liên quan đến Công ty Long Sơn, dẫn đến vụ án đau lòng. Các cán bộ liên quan cũng phải ' +
+        'kiểm điểm trách nhiệm của mình để có hướng xử lý. Vụ việc cũng được Tỉnh ủy báo cáo cho ' +
+        'Ban Bí thư Trung ương Đảng.'
+    })
+}
 
 class CommentModal extends Component {
     constructor(props) {
@@ -54,22 +44,61 @@ class CommentModal extends Component {
         }
     }
 
+    renderViewMore(onPress){
+        return(
+            <Text
+                style={{color: '#365FB7', alignSelf: 'flex-end'}}
+                onPress={onPress}>View more</Text>
+        )
+    }
+
+    renderViewLess(onPress){
+        return(
+            <Text
+                style={{color: '#365FB7', alignSelf: 'flex-end'}}
+                onPress={onPress}>View less</Text>
+        )
+    }
+
     renderRow(property) {
         return (
-            <View>
-                <Text>{property.text}</Text>
+            <View style={{flexDirection: 'row', marginTop: 5}}>
+                <View>
+                    <Image
+                        style={{height: 30, width: 30, borderRadius: 15, borderWidth: 0.5, borderColor: 'gray'}}
+                        source={{uri: property.img}}
+                        resizeMode='stretch'/>
+                </View>
+                <View style={{flexDirection: 'column', marginLeft: 5}}>
+                    <TouchableOpacity>
+                        <Text style={{fontWeight: 'bold', color: '#365FB7'}}>{property.name}</Text>
+                    </TouchableOpacity>
+                    <View style={{ width: 260}}>
+                        <ViewMoreText
+                            renderViewMore={this.renderViewMore}
+                            renderViewLess={this.renderViewLess}
+                            numberOfLines={4}>
+                            <Text style={{textAlign: 'left', flexWrap: 'wrap'}}>{property.comment}</Text>
+                        </ViewMoreText>
+                    </View>
+                </View>
             </View>
         )
     }
 
     render() {
-        var closeButton = <ButtonAPSL
-                            textStyle={{color: 'white'}}
-                            onPress={() => this.props.onClosed()}
-                            style={styles.closeButton}>X</ButtonAPSL>;
+        var closeButton = <View style={styles.backdropModal}>
+            <ButtonAPSL
+                textStyle={{color: 'white'}}
+                onPress={() => this.props.onClosed()}
+                style={styles.closeButton}>X</ButtonAPSL>
+        </View>;
 
         return (
             <Modal
+                animationDuration={200}
+                backdropOpacity={0}
+                position="center"
                 isDisable={false}
                 swipeToClose={false}
                 backButtonClose={true}
@@ -77,14 +106,37 @@ class CommentModal extends Component {
                 onClosed={() => this.props.onClosed()}
                 style={styles.modal}
                 isOpen={this.props.isOpen}>
-                <View style={{flex: 1}}>
-                    <ListView
-                        style={styles.listview}
-                        renderRow={this.renderRow.bind(this)}
-                        enableEmptySections={true}
-                        dataSource={this.state.dataSource}
-                        scrollEnabled={true}/>
-                </View>
+                <KeyboardAwareScrollView
+                    extraHeight={100}
+                    style={{ flexDirection: 'column', height: 470}}>
+                    <View style={{height: 450}}>
+                        <ListView
+                            renderSeparator={(sectionId, rowId) => <View key={rowId}
+                                                                         style={{ flex: 1
+                                                                             , height: 10
+                                                                             , borderBottomWidth: 0.5}} />}
+                            style={styles.listview}
+                            renderRow={this.renderRow.bind(this)}
+                            enableEmptySections={true}
+                            dataSource={this.state.dataSource}
+                            scrollEnabled={true}/>
+                    </View>
+                    <View style={styles.textInputConatiner}>
+                        <TextInput
+                            style={styles.textInput}
+                            underlineColorAndroid="white"
+                            placeholderTextColor='gray'
+                            placeholder='Comment about ...'
+                            returnKeyType='done'
+                            multiline={true}
+                            keyboardType="default" />
+                        <Icon
+                            size={25}
+                            color="white"
+                            style={styles.sendIcon}
+                            name="play"/>
+                    </View>
+                </KeyboardAwareScrollView>
             </Modal>
         )
     }
@@ -92,12 +144,11 @@ class CommentModal extends Component {
 
 const styles = StyleSheet.create({
     modal: {
-        height: 600,
+        height: 500,
         borderRadius: 10,
-        width: 300
+        width: 350
     },
     closeButton: {
-        position: "absolute",
         top: 0,
         right: 0,
         width: 30,
@@ -105,11 +156,41 @@ const styles = StyleSheet.create({
         backgroundColor: "transparent",
         margin: 5,
         padding: 0,
-        borderWidth: 0
+        borderWidth: 0,
+        alignSelf: 'flex-end'
     },
     listview: {
-        borderWidth: 2,
-        margin: 20
+        margin: 20,
+
+    },
+    backdropModal: {
+        width: window.width,
+        height: 650,
+        backgroundColor: 'rgba(0,0,0,0.5)'
+    },
+    textInputConatiner: {
+        height: 50,
+        backgroundColor: '#f66f88',
+        borderWidth: 0.5,
+        width: 350,
+        flexDirection: 'row',
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        borderColor: 'gray',
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0
+    },
+    textInput: {
+        flex: 6/7,
+        borderRadius: 5,
+        backgroundColor: 'white',
+        margin: 10,
+    },
+    sendIcon: {
+        flex: 1/7,
+        marginLeft: 20
     }
 });
 
